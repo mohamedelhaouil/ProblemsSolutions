bae739dce14a4ab77e59a6eae126598721ed4f7f
Mohamedelhaouil <wydad.elhaouil@gmail.com>
2023-01-28 22:45:30 +0100

    New CF Solutions

---
+++ Codeforces/B - Anton and Digits.cpp
@@ -0,0 +1,55 @@
+/*
+*    by: MEL05
+*/
+#include<bits/stdc++.h>
+/*#include<unordered_map>
+#include<unordered_set>*/
+
+using namespace std;
+void ios_b() {
+	ios_base :: sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+void f() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+#define debug(x) cerr << (#x) << " = " << x << endl;
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef unsigned long long ull;
+ll gcd(ll a, ll b) { return (!b) ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return ((a / gcd(a, b)) * b); }
+const double PI = acos(-1.0);
+const double EPS = 1e-10;
+const ll MOD = ll(1e9 + 7), INF = ll(1e9 + 7);
+int dx[] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[] = {1, -1, 0, 0, -1, 1, 1, -1};
+void solve() {
+	int k2, k3, k5, k6;
+	cin >> k2 >> k3 >> k5 >> k6;
+	ll ans = 0;
+	while(k2 && k5 && k6) {
+		ans += 256LL;
+		k2--, k5--, k6--;
+	}
+	while(k3 && k2) {
+		ans += 32LL;
+		k3--, k2--;
+	}
+	cout << ans << endl;
+}
+int main () {
+	ios_b();
+	f();
+	int t = 1;
+	// cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
