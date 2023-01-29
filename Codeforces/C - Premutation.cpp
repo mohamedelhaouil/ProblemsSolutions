0c515e5cd7a2e543e654461b6480dc937618171b
Mohamedelhaouil <wydad.elhaouil@gmail.com>
2023-01-29 01:07:23 +0100

    CF

---
+++ Codeforces/C - Premutation.cpp
@@ -0,0 +1,64 @@
+/*
+*    by: MEL05
+*/
+#include<bits/stdc++.h>
+/*#include<unordered_map>
+#include<unordered_set>*/
+
+using namespace std;
+void ios_b() {
+	ios_base :: sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+void f() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+#define debug(x) cerr << (#x) << " = " << x << endl;
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef unsigned long long ull;
+ll gcd(ll a, ll b) { return (!b) ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return ((a / gcd(a, b)) * b); }
+const double PI = acos(-1.0);
+const double EPS = 1e-10;
+const ll MOD = ll(1e9 + 7), INF = ll(1e9 + 7);
+int dx[] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[] = {1, -1, 0, 0, -1, 1, 1, -1};
+void solve() {
+	int n;
+	cin >> n;
+	vector<vector<int>> v(n,vector<int>(n - 1));
+	int cnt[140] = {0};
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n - 1; j++) {
+			cin >> v[i][j];
+			if(!j) cnt[v[i][j]]++;
+		}
+	}
+	int a = -1;
+	for(int i = 0; i < n; i++) {
+		if(cnt[v[i][0]] == 1) {
+			a = i;
+		}
+	}
+	if(a < n - 1) cout << v[a+1][0];
+	else cout << v[a-1][0];
+	for(int i = 0; i < n - 1; i++) {
+		cout << " " << v[a][i];
+	}
+	cout << endl;
+}
+int main () {
+	ios_b();
+	f();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
