e3b4203049a479b470f2af00a48c585395a766b1
Mohamedelhaouil <wydad.elhaouil@gmail.com>
2023-02-19 17:40:51 +0100

    CF

---
+++ Codeforces/1618A - Polycarp and Sums of Subsequences.cpp
@@ -0,0 +1,49 @@
+/*
+*    by: MEL05
+*/
+#include<bits/stdc++.h>
+/*#include<unordered_map>
+#include<unordered_set>*/
+
+using namespace std;
+void ios_b() {
+	ios_base :: sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+void f() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+#define debug(x) cerr << (#x) << " = " << x << endl;
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef unsigned long long ull;
+ll gcd(ll a, ll b) { return (!b) ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return ((a / gcd(a, b)) * b); }
+const double PI = acos(-1.0);
+const double EPS = 1e-10;
+const ll MOD = ll(1e9 + 7), INF = ll(1e9 + 7);
+int dx[] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[] = {1, -1, 0, 0, -1, 1, 1, -1};
+void solve() {
+	vector<int> v(7);
+	for(int i = 0; i < 7; i++) {
+		cin >> v[i];
+	}
+	cout << v[0] << " "<< v[1] << " ";
+	if(v[0] + v[1] == v[2]) cout << v[3] << endl;
+	else cout << v[2] << endl;
+}
+int main () {
+	ios_b();
+	f();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
