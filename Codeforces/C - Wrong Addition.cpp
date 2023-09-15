224b2fec879ec85d04b52bad35e016cc3db6d0d6
mohamed <wydad.elhaouil@gmail.com>
2023-09-15 15:40:52 +0000

    CF

---
+++ Codeforces/C - Wrong Addition.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+/*#include<unordered_map>
+#include<unordered_set>*/
+
+using namespace std;
+void ios_b() {
+	ios_base :: sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+void f() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+#define sz(x) (int)(x).size()
+#define all(s) (s).begin(), (s).end()
+#define rall(s) (s).rbegin(), (s).rend()
+#define debug(x) cerr << (#x) << " = " << x << endl;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ll gcd(ll a, ll b) { return (!b) ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return ((a / gcd(a, b)) * b); }
+const double PI = acos(-1.0);
+const double EPS = 1e-10;
+const ll MOD = ll(1e9 + 7), INF = ll(1e9 + 7);
+int dx[] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[] = {1, -1, 0, 0, -1, 1, 1, -1};
+void solve() {
+	ll a, s;
+	cin >> a >> s;
+	bool ok = true;
+	string ans = "";
+	while(a && s) {
+		int x, y;
+		x = a % 10;
+		y = s % 10;
+		a /= 10;
+		s /= 10;
+		if(y - x < 0) {
+			if(s) {
+				y = (s % 10) * 10 + y;
+				s /= 10;
+				if(y < 10 || y > 18) {
+					ok = false;
+					break;
+				}
+			}
+			else {
+				ok = false;
+				break;
+			}
+		}
+		ans += (y - x) + '0';
+	}
+	while(s) {
+		int y = s % 10;
+		s /= 10;
+		ans += y + '0';
+	}
+	reverse(all(ans));
+	ok &= !a;
+	cout << (ok ? stoll(ans) : -1) << endl;
+}
+int main () {
+	ios_b();
+	f();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
